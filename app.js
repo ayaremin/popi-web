a28df89b5f960ab53a83998c22e341e4c2b40257
Muhammed Emin Ayar <emin@omoody.com>
2018-03-05 21:42:18 +0300

    Started changing design of admin panel

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var vhost = require('vhost')
+var vhost = require('vhost');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
