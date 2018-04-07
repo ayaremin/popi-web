b702e374bc3bd312973a223c255f0635f7114194
Muhammed Emin Ayar <emin@omoody.com>
2018-04-07 17:16:18 +0300

    Change db path to explicit

--- migrate/fire.js
+++ migrate/fire.js
@@ -22,7 +22,7 @@ var interactionsReference = db.ref('/interactions');
 
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.POPIDB);
+mongoose.connect('mongodb://popi-root:root-Pss!3020popi@104.236.87.130:28541/popiDatabase');
 
 mongoose.connection.on('connected', function () {
     // after a succesful connection start listening firebase
